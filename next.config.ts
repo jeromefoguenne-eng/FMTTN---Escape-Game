2ae50cc48e3bef4ecb656d86c162d9c03e7d42f6
jeromefoguenne-eng <jeromefoguenne@gmail.com>
2026-09-11 23:05:44 +0200

    chore: add trailingSlash: true for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,7 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: basePath,
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
